e2fe35dd77e271a31c60d7b24e145bb326c16977
AidarT <75251421+AidarT@users.noreply.github.com>
2020-12-16 14:04:39 +0300

    Refactoring

--- aoc2020day16.js
+++ aoc2020day16.js
@@ -9,64 +9,40 @@ function day16() {
     });
     let rules = {}
     input.split("your ticket:")[0].split("\r\n").filter(a => a !== "")
-        .map(rule => {
+        .forEach(rule => {
             const [left, right] = rule.split(": ");
-            rules[left] = [parseInt(right),
-                parseInt(right.split("-")[1]),
-                parseInt(right.split(" or ")[1]),
-                parseInt(right.split(" or ")[1].split("-")[1])]
+            rules[left] = [...right.match(/\d+/g).map(Number)]
         })
-    let your_t = input.split("your ticket:")[1].split("nearby tickets:")[0]
-        .split("\r\n").filter(a => a !== "")
-    let nrby_t = input.split("your ticket:")[1].split("nearby tickets:")[1]
-        .split("\r\n").filter(a => a !== "")
+    const your_t = input.split("your ticket:")[1].split("nearby tickets:")[0]
+        .split("\r\n").filter(a => a !== "").map(a => [...a.match(/\d+/g).map(Number)])[0]
+    let nrby_t = input.split("nearby tickets:")[1]
+        .split("\r\n").filter(a => a !== "").map(a => [...a.match(/\d+/g).map(Number)])
 
-    let red = []
-    let part1 = nrby_t.reduce((prev, cur, i) => {
-        cur.split(",").forEach(a => {
-            a = parseInt(a)
-            let key = 0
-            for (let rule in rules) {
-                if (!(rules[rule][0] <= a && rules[rule][1] >= a) && !(rules[rule][2] <= a && rules[rule][3] >= a)) {
-                    key++
-                }
-            }
-            if (key === Object.keys(rules).length) {
-                prev += a
-                red.push(i)
-            }
-        })
-        return prev
-    }, 0)
+    const part1 = nrby_t.reduce((sum, cur) =>
+        sum + cur.filter(t => Object.keys(rules).every(r =>
+            !(rules[r][0] <= t && rules[r][1] >= t) && !(rules[r][2] <= t && rules[r][3] >= t)))
+            .reduce((sum, valid) => sum + valid,0)
+    , 0)
 
-    for (let i = nrby_t.length - 1; i >= 0; i--) {
-        if (red.includes(i)) {
-            nrby_t.splice(i, 1)
-        }
-    }
+    nrby_t = nrby_t.filter(cur => cur.every(t => !Object.keys(rules).every(r =>
+        !(rules[r][0] <= t && rules[r][1] >= t) && !(rules[r][2] <= t && rules[r][3] >= t)))
+    )
 
-    let rule_names = []
-    let j = 0
-    while (j < nrby_t[0].split(",").length) {
+    let rule_names = [], j = 0
+    while (j < nrby_t[0].length) {
         rule_names[j] = []
-        for (let rule in rules) {
-            let key = 0
-            nrby_t.forEach((cur, i) => {
-                let a = cur.split(",")[j]
-                a = parseInt(a)
-                if (!(rules[rule][0] <= a && rules[rule][1] >= a) && !(rules[rule][2] <= a && rules[rule][3] >= a)) {
-                    key++
-                }
-            })
-            if (key === 0 && !rule_names[j].includes(rule)) {
-                rule_names[j].push(rule)
+        for (let r in rules) {
+            let key = nrby_t.every(t =>
+                (rules[r][0] <= t[j] && rules[r][1] >= t[j]) || (rules[r][2] <= t[j] && rules[r][3] >= t[j])            )
+            if (key === true && !rule_names[j].includes(r)) {
+                rule_names[j].push(r)
             }
         }
         j++
     }
 
     j = 1
-    while (true) {
+    while (!rule_names.every(rule => rule.length === 1)) {
         rule_names.forEach((rule, ind) => {
             if (rule.length === 1) {
                 rule_names.forEach((el, i) => {
@@ -76,18 +52,10 @@ function day16() {
                 })
             }
         })
-        if (rule_names.reduce((prev,cur) => {return prev + cur.length},0) === rule_names.length) {
-            break
-        }
     }
 
-    let part2 = 1
-    rule_names.forEach((a, i) => {
-        if (a[0].includes("departure")) {
-            let num = parseInt(your_t[0].split(",")[i])
-            part2 *= num
-        }
-    })
+    const part2 = your_t.filter((a,i) => rule_names[i][0].includes("departure"))
+        .reduce((prev,cur) => prev * cur, 1)
 
     console.log(part1 + " " + part2)
 }
