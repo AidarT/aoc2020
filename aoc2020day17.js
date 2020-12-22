fbc7b079e628a2c4950d7c80b393c90f3623aecf
AidarT <aidar.tu.n@gmail.com>
2020-12-23 00:52:48 +0300

    Day 17 Solvation

--- aoc2020day17.js
+++ aoc2020day17.js
@@ -3,15 +3,36 @@ const fs = require('fs');
 day17()
 
 function day17() {
-    let input = fs.readFileSync('C:\\Users\\User\\Documents\\input.txt', "utf8")
+    let input = fs.readFileSync('C:\\Users\\Hazard\\Documents\\input.txt', "utf8")
 
     let cube = [input.split("\r\n").map(a => a.split(""))]
     cube.push(cube[0].map(a => a.map(a => ".")))
     cube.push(cube[1])
-    cube[1] = cube[0].slice(); cube[0] = cube[2].slice();
+    // cube[1] = cube[0].slice(); cube[0] = cube[2].slice();
+    cube[1] = JSON.parse(JSON.stringify(cube[0])); cube[0] = JSON.parse(JSON.stringify(cube[2]));
 
+    const dir = [
+        [-1, -1, -1], [-1, -1, 0], [-1, -1, 1],
+        [-1, 0, -1], [-1, 0, 0], [-1, 0, 1],
+        [-1, 1, -1], [-1, 1, 0], [-1, 1, -1],
+        [0, -1, -1], [0, -1, 0], [0, -1, 1],
+        [0, 0, -1], [0, 0, 1],
+        [0, 1, -1], [0, 1, 0], [0, 1, 1],
+        [1, -1, -1], [1, -1, 0], [1, -1, 1],
+        [1, 0, -1], [1, 0, 0], [1, 0, 1],
+        [1, 1, -1], [1, 1, 0], [1, 1, -1]
+    ]
 
-    const dir = []
+    cube.push(cube[0].map(a => a.map(a => ".")))
+    cube.unshift(cube[0].map(a => a.map(a => ".")))
+    cube.forEach(a => {
+        a.forEach(a => {
+            a.push(".");
+            a.unshift(".")
+        })
+        a.push(a[0].map(a => "."));
+        a.unshift(a[0].map(a => "."))
+    })
 
     const part1 = 0
 
