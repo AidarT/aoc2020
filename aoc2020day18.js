5424fca033a410a104c52106e683a29937c1488f
Aidar Tukmeev <aidar.tu.n@gmail.com>
2020-12-25 15:05:06 +0300

    Day 18 Solvation

--- aoc2020day18.js
+++ aoc2020day18.js
@@ -31,26 +31,32 @@ function day18() {
     let part1 = nums.reduce((prev, cur,i) => {
         return prev + eq_solve(cur, signs[i])}, 0)
 
-    for (let i = 0; i < newInput.length; i++) {
-        while(true) {
-            let sums = newInput[i].match(/\d+\s\+\s\d+/g)
-            if (sums === null) {break}
-            sums.forEach((a) => {
-                if (a !== null) {
-                    let memb = a.match(/\d+/g).map(Number)
-                    let answ = memb.reduce((prev, cur) => prev + cur, 0)
-                    newInput[i] = newInput[i].replace(a, answ.toString())
-                }
-            })
-        }
-    }
-
-    newInput = newInput.map((a, i) => {
-        if (a.match(/\(\d+\)/g) !== null) {
-            let b = a.match(/\(\d+\)/g).map(a => a.match(/\d+/))
-            newInput[i] = a.replace(/\(\d+\)/g, a.match(/\(\d+\)/g).map(a => a.match(/\d+/g)))
-        }
-    })
+    // for (let i = 0; i < newInput.length; i++) {
+    //     while(true) {
+    //         let sums = newInput[i].match(/\d+\s\+\s\d+/g)
+    //         if (sums === null) {break}
+    //         sums.forEach((a) => {
+    //             if (a !== null) {
+    //                 let memb = a.match(/\d+/g).map(Number)
+    //                 let answ = memb.reduce((prev, cur) => prev + cur, 0)
+    //                 newInput[i] = newInput[i].replace(a, answ.toString())
+    //             }
+    //         })
+    //     }
+    // }
+    //
+    // newInput.forEach((a, i) => {
+    //     if (a.match(/\(\d+\)/g) !== null) {
+    //         let b = a.match(/\(\d+\)/g).map(a => a.match(/\d+/g))
+    //         if (b.length < 2) {
+    //             newInput[i] = a.replace(/\(\d+\)/g, a.match(/\(\d+\)/g).map(a => a.match(/\d+/g)))
+    //         } else {
+    //             for (let j = 0; j < b.length; j++) {
+    //                 newInput[i] = newInput[i].replace("(" + b[j][0] + ")", b[j][0])
+    //             }
+    //         }
+    //     }
+    // })
 
     while(true) {
         let parentheses = newInput.map(a => a.match(/\((\d+\W{1,3})+\d+\):?/g))
@@ -104,8 +110,6 @@ function day18() {
     let part2 = newInput.reduce((prev, cur) => {
         return prev + cur.match(/\d+/g).map(Number).reduce((prev, cur) => prev * cur, 1)
     }, 0)
-
-    //newInput = newInput.map(a => a.match(/\d+/g).map(Number).reduce((prev, cur) => prev * cur, 1))
 
     console.log(part1 + " " + part2)
 }
