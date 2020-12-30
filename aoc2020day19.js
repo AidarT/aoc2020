a7efd20c67dc440ee10b48204effb7eab4b71773
Aidar Tukmeev <aidar.tu.n@gmail.com>
2020-12-30 15:28:57 +0300

    Day 19 Solvation

--- aoc2020day19.js
+++ aoc2020day19.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 day19()
 
 function day19() {
-    let input = fs.readFileSync('C:\\Users\\Hazard\\Documents\\input.txt', "utf8")
-
+    let input = fs.readFileSync('C:\\Users\\User\\Documents\\input.txt', "utf8")
     input = input.split("\r\n\r\n");
 
     let messages = input[1].split("\r\n");
@@ -15,9 +14,32 @@ function day19() {
         rules[parseInt(num)] = rule
     })
 
-    let part1 = 0
+    let reg_exp = rule_ins("0", rules, 1).replace(/\s/g, "")
+    reg_exp = new RegExp("^" + reg_exp + "$", "g");
+    let part1 = messages.filter(a => a.match(reg_exp) !== null)
 
-    let part2 = 0
+    rules[8] = "42 | 42 8"
+    rules[11] = "42 31 | 42 11 31"
 
-    console.log(part1 + " " + part2)
+    reg_exp = rule_ins("0", rules, 1).replace(/\s/g, "")
+    reg_exp = new RegExp("^" + reg_exp + "$", "g");
+    let part2 = messages.filter(a => a.match(reg_exp) !== null)
+
+    console.log(part1.length + " " + part2.length)
+}
+
+function rule_ins(start, rules, cycles) {
+    start = start.split(" ")
+    for (let i = 0; i < start.length; i++) {
+        if (start[i].match(/[ab\|]/) === null) {
+            if (cycles < 50) {
+                start[i] = rule_ins(rules[start[i]], rules, cycles + 1)
+            }
+        }
+    }
+    if (start.includes("|")) {
+        return "(" + start.join(" ") + ")"
+    } else {
+        return start.join(" ")
+    }
 }
