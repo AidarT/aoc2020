3ab3a58a927f770bc54cfa3c95aa76ecf58b5573
AidarT <aidar.tu.n@gmail.com>
2021-01-10 20:42:10 +0300

    Day 21 Solvation

---
+++ aoc2020day21.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+day21()
+
+function day21() {
+    let input = fs.readFileSync('C:\\Users\\Hazard\\Documents\\input.txt', "utf8").split("\r\n")
+
+    let part1 = 0
+
+    let part2 = 0
+
+    console.log(part1 + " " + part2)
+}
+
